84856054433697b9b09834886aa81a5d849d0641
CyberBravo <CyberBravo@users.noreply.github.com>
2019-03-28 01:24:02 +0500

    update

---
+++ Assets/BallCollision.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollision : MonoBehaviour {
+
+	public Transform NewContainer;
+	public float forceFactor = 10;
+
+
+	void Start()
+	{
+		NewContainer = GameObject.Find ("NewContainer").transform;
+	}
+
+
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Slider" && gameObject.tag == "Ball") 
+		{
+			gameObject.tag = "Passed";
+			print ("___________________________");
+
+			print ("Ball " + gameObject.name);
+			print ("Slider " + collision.gameObject.name);
+
+			if (collision.gameObject.name.Contains (gameObject.name))
+			{
+				FindObjectOfType<BallFallController> ().IncreaseScroe (1);
+				print ("Score increment");
+			} 
+			else if (!collision.gameObject.name.Contains (gameObject.name))
+			{
+				collision.gameObject.AddComponent<SwipeController> ();
+				Time.timeScale = 0;
+				print ("Score decrement");
+				FindObjectOfType<BallFallController> ().DecreaseScroe (1);
+			}
+		}
+//		if (collision.gameObject.name == "pink" || collision.gameObject.name == "Red" || collision.gameObject.name == "green"
+//			|| collision.gameObject.name == "yellow" || collision.gameObject.name == "blue" || collision.gameObject.name == "Orange") 
+//		{
+//
+////			GetComponent<Rigidbody>().AddForce((Magnet.transform.position - transform.position) * forceFactor * Time.smoothDeltaTime);
+//			transform.SetParent(NewContainer);
+//			//print (collision.gameObject.name);
+//			GetComponent<Rigidbody> ().drag = 0.01f;
+//			gameObject.layer = LayerMask.NameToLayer("Default");
+//		}
+	}
+
+	void OnTriggerEnter(Collider collision)
+	{
+
+
+		if (collision.gameObject.name == "door") 
+		{
+
+			transform.SetParent(NewContainer);
+			GetComponent<Rigidbody> ().drag = 0.01f;
+			gameObject.layer = LayerMask.NameToLayer("Default");
+		}
+	}
+
+}
---
+++ Assets/Scripts/BallFallController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BallFallController : MonoBehaviour {
+
+	// Use this for initialization
+	public Text scroe;
+
+	public Material [] mats;
+	public GameObject ball;
+	void Start () {
+
+		StartCoroutine (CreateBalls ());
+
+	}
+
+	public Transform slide;
+
+	IEnumerator CreateBalls()
+	{
+		while (true) {
+		
+			GameObject g = Instantiate (ball , slide );
+			g.transform.localScale = new Vector3 (0.6f, 0.6f, 0.6f);
+			int i = Random.Range (0, mats.Length);
+			g.GetComponent<Renderer>().material = mats [i];
+			g.name = mats [i].name;
+			Destroy (g, 10f);
+
+			yield return new WaitForSeconds(2f);
+		
+		}
+
+	}
+
+
+
+	public void IncreaseScroe(int score)
+	{
+		Variables.score += score;
+		this.scroe.text = Variables.score.ToString();
+	}
+	public void DecreaseScroe(int score)
+	{
+		Variables.score -= score;
+		this.scroe.text = Variables.score.ToString();
+		if (int.Parse (this.scroe.text.ToString ()) <= -10) 
+		{
+			Time.timeScale = 0;
+			this.scroe.text = "GameOver";
+		}
+	}
+
+
+	public void tim()
+	{
+		Time.timeScale = 1f;
+	}
+
+
+}
