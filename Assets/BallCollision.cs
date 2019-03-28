2f01ae8ec96f7e9f85739619aa614966073f50b1
CyberBravo <CyberBravo@users.noreply.github.com>
2019-03-28 20:40:12 +0500

    bug fixed

--- Assets/BallCollision.cs
+++ Assets/BallCollision.cs
@@ -15,52 +15,26 @@ public class BallCollision : MonoBehaviour {
 
 
 
-	void OnCollisionEnter(Collision collision)
-	{
-		if (collision.gameObject.tag == "Slider" && gameObject.tag == "Ball") 
-		{
-			gameObject.tag = "Passed";
-			print ("___________________________");
-
-			print ("Ball " + gameObject.name);
-			print ("Slider " + collision.gameObject.name);
-
-			if (collision.gameObject.name.Contains (gameObject.name))
-			{
-				FindObjectOfType<BallFallController> ().IncreaseScroe (1);
-				print ("Score increment");
-			} 
-			else if (!collision.gameObject.name.Contains (gameObject.name))
-			{
-				collision.gameObject.AddComponent<SwipeController> ();
-				Time.timeScale = 0;
-				print ("Score decrement");
-				FindObjectOfType<BallFallController> ().DecreaseScroe (1);
-			}
-		}
-//		if (collision.gameObject.name == "pink" || collision.gameObject.name == "Red" || collision.gameObject.name == "green"
-//			|| collision.gameObject.name == "yellow" || collision.gameObject.name == "blue" || collision.gameObject.name == "Orange") 
-//		{
-//
-////			GetComponent<Rigidbody>().AddForce((Magnet.transform.position - transform.position) * forceFactor * Time.smoothDeltaTime);
-//			transform.SetParent(NewContainer);
-//			//print (collision.gameObject.name);
-//			GetComponent<Rigidbody> ().drag = 0.01f;
-//			gameObject.layer = LayerMask.NameToLayer("Default");
-//		}
-	}
-
 	void OnTriggerEnter(Collider collision)
 	{
 
-
-		if (collision.gameObject.name == "door") 
+		if (collision.CompareTag ("door")) 
 		{
-
 			transform.SetParent(NewContainer);
 			GetComponent<Rigidbody> ().drag = 0.01f;
 			gameObject.layer = LayerMask.NameToLayer("Default");
+
+			if (collision.name == gameObject.name) 
+			{
+				//print ("Matched");
+				FindObjectOfType<BallFallController> ().IncreaseScroe (1);
+			}
+			else
+				FindObjectOfType<BallFallController> ().DecreaseScroe (1);
+
 		}
+
+
 	}
 
 }
